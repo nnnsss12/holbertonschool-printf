67c2329a0bd9e253c8f24577c7d4f2caa7ff0df4
nnnsss12 <ms.nora1424@gmail.com>
2026-04-14 11:21:47 +0000

    Implement _printf engine

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else if (format[i] == '\0')
+				return (-1);
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
